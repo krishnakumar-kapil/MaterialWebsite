652bfbd00a20f7b9ac9974890c6f7dd9315b3783
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2015-09-10 14:57:19 -0500

    Added colors to skills and removed d3

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
+const skillCard = '#skillContainer .mdl-parentSkill-card .mdl-grid .mdl-skill-card.mdl-cell';
+const supportinText = ' .mdl-card__supporting-text';
+var colorSkill = {"Expert": "#4CAF50","Proficient": "#2196F3","Familiar": "Orange","Exposure": "#F44336",
+					};
+
 $(document).ready(function() {
 
 	toggle('#readMoreBreak', "#displayMoreBreak");
 	toggle('#readMoreRailway', "#displayMoreRailway");
 	toggle('#readMoreMuseum', '#displayMoreMuseum');
+
+	addColorSkill();
 });
 
 function toggle(buttonName, divName){
@@ -13,5 +20,16 @@ function toggle(buttonName, divName){
 		} else{
 			$(buttonName).html("Read Less");
 		}
+	});
+}
+
+function addColorSkill(){
+	$(skillCard).each(function(){
+
+		var level = $(this).children('div.mdl-card__supporting-text').text().trim();
+		console.log(level);
+		console.log(colorSkill[level]);
+		$(this).css('background-color', colorSkill[level]);
+		// console.log(level);
 	});
 }
