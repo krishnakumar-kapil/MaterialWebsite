a6a50d9dd56605e051afa07aedb75fcbf9b5eeba
krishnakumar-kapil <krishnakumar.kapil@utexas.edu>
2015-09-10 20:27:57 -0500

    Added transitions and colors to sidebar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 const skillCard = '#skillContainer .mdl-parentSkill-card .mdl-grid .mdl-skill-card.mdl-cell';
 const supportinText = ' .mdl-card__supporting-text';
-var colorSkill = {"Expert": "#4CAF50","Proficient": "#2196F3","Familiar": "Orange","Exposure": "#F44336",
+//light
+// var colorSkill = {"Expert": "#66BB6A",
+// 					"Proficient": "#90CAF9",
+// 					"Familiar": "#FFCC80",
+// 					"Exposure": "#FFF59D",
+// 					};
+
+//dark
+// var colorSkill = {"Expert": "#1B5E20",
+// 					"Proficient": "#0D47A1",
+// 					"Familiar": "#311B92",
+// 					"Exposure": "#3E2723",
+// 					};
+
+//medium
+var colorSkill = {"Expert": "#8BC34A",
+					"Proficient": "#FFC107",
+					"Familiar": "#03A9F4",
+					"Exposure": "#00BCD4",
 					};
 
 $(document).ready(function() {
+	changeActive();
 
 	toggle('#readMoreBreak', "#displayMoreBreak");
 	toggle('#readMoreRailway', "#displayMoreRailway");
@@ -30,5 +49,13 @@ function addColorSkill(){
 		var level = $(this).children('div.mdl-card__supporting-text').text().trim();
 		$(this).css('background-color', colorSkill[level]);
 		// console.log(level);
+	});
+}
+
+function changeActive(){
+	var loc = window.location.pathname;
+	console.log(loc);
+	$('#topBar').find('a').each(function() {
+	  $(this).toggleClass('active', $(this).attr('href') == loc);
 	});
 }
